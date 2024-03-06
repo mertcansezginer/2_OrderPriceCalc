bbe0f93577de7442c6d2e3b18fcb60358349564e
mertc <mertc@DESKTOP-8ID65LJ>
2024-03-06 14:12:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+string[] urunler = { "kalem", "silgi", "defter" }; //fiyat soracagimiz urunlerin isimlerini iceren array olusturduk
+double[] fiyatlar = new double[3]; // urunlerin fiyatlarini saklamak icin array olusturduk
+double ToplamFiyat = 0;            // toplam fiyatin tutulacagi bir variable belirledik
+
+for (int i = 0; i < urunler.Length; i++)
+{
+    
+    Console.WriteLine("Kac adet " + urunler[i] + " alinacak?"); //adet bilgisi alma
+    int adet = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Fiyati ne kadar?");                       //her urun icin fiyat bilgisi alma
+    double fiyat = double.Parse(Console.ReadLine());
+
+    fiyatlar[i] = adet * fiyat;          //toplam fiyat hesaplama 
+    ToplamFiyat += fiyatlar[i];
+
+}
+
+Console.WriteLine ("Toplam alisveris tutari : " +  ToplamFiyat + " TL");
+
+
+
